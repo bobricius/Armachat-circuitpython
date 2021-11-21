11bd36d6108207b93fe0bcfd38cce191311cfa83
Peter Misenko <bobricius@gmail.com>
2021-11-22 00:09:40 +0100

    Safe mode, drive rename
    +Safe mode, If you press on boot ESC, user program not started 
    +drive reamed to ARMACHAT
    +USB detection ... for future disable disk write protection ...

--- Software/boot.py
+++ Software/boot.py
@@ -4,6 +4,7 @@ import busio
 import gc
 import terminalio
 import displayio
+import digitalio
 import microcontroller
 import storage
 
@@ -12,6 +13,20 @@ from digitalio import DigitalInOut
 
 from adafruit_display_text import label
 from adafruit_st7789 import ST7789
+
+
+LED = digitalio.DigitalInOut(board.LED)
+LED.direction = digitalio.Direction.OUTPUT
+
+safeGND = digitalio.DigitalInOut(board.GP7)
+safeGND.direction = digitalio.Direction.OUTPUT
+safeGND.value = False
+
+VBUS_status = digitalio.DigitalInOut(board.VBUS_SENSE) # defaults to input
+VBUS_status.pull = digitalio.Pull.UP # turn on internal pull-up resistor
+
+safe = digitalio.DigitalInOut(board.GP14)  # <-- choose your button pin
+safe.pull = digitalio.Pull.UP
 
 # Release any resources currently in use for the displays
 displayio.release_displays()
@@ -34,7 +49,31 @@ display = ST7789(
 # splash = displayio.Group()
 # display.show(splash)
 
+
 print ("Free memory:")
 print (gc.mem_free())
-print ("Starting code.py...")
 
+if VBUS_status.value :
+	print("USB power connected")
+else:
+	print ("No USB power")
+
+print ("Press ESC for SAFE MODE:")
+LED.value = False
+for x in range(16):
+	LED.value = not LED.value  # toggle LED on/off as notice
+	time.sleep(0.05)
+	if safe.value is False:
+		print ("SAFE MODE DETECTED .....")
+		microcontroller.on_next_reset(microcontroller.RunMode.SAFE_MODE)
+		microcontroller.reset()
+
+# RENAME DRIVE
+new_name = "ARMACHAT"
+
+storage.remount("/", readonly=False)
+m = storage.getmount("/")
+m.label = new_name
+storage.remount("/", readonly=True)
+
+print ("Starting code.py...")
